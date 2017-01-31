45cba67a5aed9f8f8b55a3f7a2d8e870b433e217
Jee Wan Gue <jeewangue@gmail.com>
2017-01-31 20:41:19 +0900

    elastic

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@
 
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential git zsh tmux openssl nginx autojump vim tree htop imagemagick libmysqlclient-dev sqlite3 graphviz 
+sudo apt-get install build-essential git zsh tmux openssl nginx autojump vim tree htop imagemagick libmysqlclient-dev sqlite3 graphviz nodejs
+sudo apt-get install postgresql postgresql-server-dev-all redis-server
 
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -24,3 +25,16 @@ rvm install 2.3.3
 gem install pry
 gem install rails
 gem install passenger
+
+# elastic
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update
+sudo apt-get install elasticsearch
+sudo apt-get install logstash
+sudo apt-get install kibana
+sudo apt-get install filebeat
+sudo apt-get install metricbeat
+sudo apt-get install packetbeat
+
