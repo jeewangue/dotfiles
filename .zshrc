36b2e0c002c131930cddbdd02e7f74ee2007ce9f
jeewangue <jeewangue@gmail.com>
2019-11-25 19:36:25 +0900

    powerlevel10k

--- .zshrc
+++ .zshrc
@@ -1,7 +1,6 @@
 export ZSH=$HOME/.oh-my-zsh
 
-ZSH_THEME="spaceship"
-SPACESHIP_TIME_SHOW=true
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 HISTSIZE=100000
 HIST_STAMPS="mm/dd/yyyy"
--- setup.sh
+++ setup.sh
@@ -67,6 +67,8 @@ if [ "$answer" != "${answer#[Yy]}" ]; then
   git clone https://github.com/denysdovhan/spaceship-prompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt
   ln -s ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship-prompt/spaceship.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/spaceship.zsh-theme
 
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
