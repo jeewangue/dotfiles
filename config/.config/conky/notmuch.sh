321a593d97196a3199817f0d3e27313661e99cf6
Jee Wan Gue <jeewangue@gmail.com>
2021-01-25 04:19:29 +0900

    conky notmuch

--- config/.config/conky/notmuch.sh
+++ config/.config/conky/notmuch.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-recent_mails=$(notmuch search --format json --output summary --limit 10 to:"jeewangue@gmail.com" tag:inbox tag:unread | jq --join-output '.[] | "\\n${color2}Subject:${color} \(.subject | .[0:80])\\n${color2}Time:${color} \(.timestamp | strflocaltime("%F %H:%M"))  ${color2}From:${color} \(.authors)\\n"')
+address=${1:-jeewangue@gmail.com}
+count=${2:-10}
+
+recent_mails=$(notmuch search --format json --output summary --limit $count to:"$address" tag:inbox tag:unread | jq --join-output '.[] | "\\n${color2}Subject:${color} \(.subject | .[0:80])\\n${color2}Time:${color} \(.timestamp | strflocaltime("%F %H:%M")) ${color2}From:${color} \(.authors)\\n"')
 
 echo -ne $recent_mails
