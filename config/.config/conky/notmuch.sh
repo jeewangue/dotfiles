9e408dc61791c83800da8f1317475ec740cbc502
Jee Wan Gue <jeewangue@gmail.com>
2021-01-25 04:02:38 +0900

    conky notmuch & i3 floating jetbrains

--- config/.config/conky/notmuch.sh
+++ config/.config/conky/notmuch.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
-recent_mails=$(notmuch search --format json --output summary --limit 20 to:"jeewangue@gmail.com" tag:inbox tag:unread | jq -r '.[] | "\(.timestamp | strflocaltime("%F %H:%M:%S")) \(.authors) \(.subject)"')
+recent_mails=$(notmuch search --format json --output summary --limit 10 to:"jeewangue@gmail.com" tag:inbox tag:unread | jq --join-output '.[] | "\\n${color2}Subject:${color} \(.subject | .[0:80])\\n${color2}Time:${color} \(.timestamp | strflocaltime("%F %H:%M"))  ${color2}From:${color} \(.authors)\\n"')
 
-echo -e $recent_mails
+echo -ne $recent_mails
