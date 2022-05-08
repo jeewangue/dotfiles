5baa44e4068b2dec30444a05079b4e3e4ca1f2ae
Jee Wan Gue <jeewangue@gmail.com>
2022-05-08 17:47:01 +0900

    update

--- scripts/backup-rsync.sh
+++ scripts/backup-rsync.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-BACKUP_DIR=jeewangue@192.168.83.117:/volume1/data/backup/asus-20220208
+BACKUP_DIR=jeewangue@192.168.83.117:/volume1/data/backup/asus-20220507
 
 # backup /home
 sudo rsync \
