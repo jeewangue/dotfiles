6f6f983f5d4fa679c45fc2969bdc9235a6cbd272
Jee Wan Gue <jeewangue@gmail.com>
2022-09-21 01:24:26 +0900

    update

--- scripts/update-secret.sh
+++ scripts/update-secret.sh
@@ -6,6 +6,5 @@ ROOTDIR=$(realpath "${SCRIPTDIR}/..")
 pushd ${ROOTDIR}
 
 tar cz taskbook/ | gpg -esar jeewangue@gmail.com -o "${ROOTDIR}/taskbook.tar.gz.asc"
-tar cz openvpn-root/ | gpg -esar jeewangue@gmail.com -o "${ROOTDIR}/openvpn-root.tar.gz.asc"
 
 popd
